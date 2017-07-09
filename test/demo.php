8f6b52079d7208f34b73cf6223228db5f6c76c9f
leoliu <leoliu@leoliudeMacBook-Pro.local>
2017-07-09 23:29:25 +0800

    empty与isset

--- test/demo.php
+++ test/demo.php
@@ -2,5 +2,25 @@
 $a = 10;
 $b = 20;
 
+$c = '0'; // true
+$c = '';//true
+$c = '0.0';//empty= false
+
+$c = 0.0;//true
+$c = 0;//true
+$c = null;//true
+$c = array();//true
+
+if(empty($c)){
+    echo '\'0\''.' is empty<br>';
+} else {
+    echo '\'0\''.'is not empty<br>';
+}
+
+if(isset($c)){
+    echo '\'0\''.' is set<br>';
+} else {
+    echo '\'0\''.' is not set<br>';
+}
 
 phpinfo();
