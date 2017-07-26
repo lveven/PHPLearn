2fc0020941232118de6721e82f81279e55e90df5
leoliuyt <leoliuyt@sina.cn>
2017-07-26 19:55:34 +0800

    登陆

--- moocshop/admin/main.php
+++ moocshop/admin/main.php
@@ -13,8 +13,8 @@
 			<td><?php echo PHP_OS;?></td>
 		</tr>
 		<tr>
-			<th>Apache版本</th>
-			<td><?php echo apache_get_version();?></td>
+			<th>Zend 引擎版本</th>
+			<td><?php echo zend_version();?></td>
 		</tr>
 		<tr>
 			<th>PHP版本</th>
--- moocshop/core/admin.inc.php
+++ moocshop/core/admin.inc.php
@@ -1,17 +1,32 @@
 <?php
 require_once('../include.php');
+/**
+ * 检查用户是否存在
+ *
+ * @param [type] $sql
+ * @return void
+ */
 function checkAdmin($sql){
     $sqlManager = new MySqlManager();
     // return fetchOne($sql);
     return $sqlManager->fetchOne($sql);
 }
-
+/**
+ * 检查是否登陆
+ *
+ * @return void
+ */
 function checkLogined(){
     if($_SESSION['adminId']=="" && $_COOKIE['adminId']==""){
         alertMsg("请登录","loginnew.php");
     }
 }
 
+/**
+ * 注销用户
+ *
+ * @return void
+ */
 function logout(){
     $_SESSION=array();
     if(isset($_COOKIE[session_name()])){
@@ -25,4 +40,18 @@ function logout(){
     }
     session_destroy();  
     header("location:loginnew.php");
+}
+/**
+ * 添加管理员
+ *
+ * @return void
+ */
+function addAdmin(){
+    $arr = $_POST;
+    $sqlManager = new MySqlManager();
+    if($sqlManager->insert('imooc_admin',$arr)){
+        $msg = "添加成功!<br/><a href='addAdmin.php'>继续添加</a> | <a href='listAdmin.php'>查看管理员列表</a>";
+    }else {
+        $msg = "添加失败!<br/><a href='addAdmin.php'>重新添加</a>";
+    }
 }
