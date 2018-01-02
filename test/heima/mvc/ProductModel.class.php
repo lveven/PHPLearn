501b5f59cd0d72e401b060d86d93d8fef09458aa
leoliuyt <leoliuyt@sina.cn>
2018-01-02 14:01:15 +0800

    商品

--- test/heima/mvc/ProductModel.class.php
+++ test/heima/mvc/ProductModel.class.php
@@ -2,7 +2,7 @@
 require_once './BaseModel.class.php';
 class ProductModel extends BaseModel{
     function getAllProduct(){
-        $sql = "select * from product";
+        $sql = "SELECT p.*,t.protype_name FROM product AS p INNER JOIN product_type AS t ON p.protype_id = t.protype_id";
         return $this->db->getRows($sql);
     }
 }
