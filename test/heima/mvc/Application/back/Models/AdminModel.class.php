187679f9f67f7a46a099004a1151bca3db2bcd5a
leoliuyt <leoliuyt@sina.cn>
2018-01-03 19:02:10 +0800

    检查用户

--- test/heima/mvc/Application/back/Models/AdminModel.class.php
+++ test/heima/mvc/Application/back/Models/AdminModel.class.php
@@ -1,5 +1,13 @@
 <?php
 require_once './Framework/BaseModel.class.php';
 class AdminModel extends BaseModel{
-
+    function checkAdmin($user,$pwd){
+        $sql = "SELECT COUNT(*) AS c FROM admin_user where admin_name='$user' AND admin_pass=md5('$pwd');";
+        $result = $this->db->getOneData($sql);
+        if($result === 1){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
